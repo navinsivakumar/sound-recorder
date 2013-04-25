60b0c13c13c00daa996768882d5c48d155246e8e
Navin Sivakumar <navin.sivakumar@gmail.com>
2013-04-25 19:04:45 -0400

    Create dummy layouts for record and play fragments

--- SoundRecorder/src/com/example/soundrecorder/PlayFragment.java
+++ SoundRecorder/src/com/example/soundrecorder/PlayFragment.java
@@ -1,14 +1,25 @@
 /**
- * 
+ * A simple playback interface for recordings made by the field recorder
  */
 package com.example.soundrecorder;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * @author navin
  *
  */
 public class PlayFragment extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.fragment_play, container, false);
+	}
 
 }
--- SoundRecorder/src/com/example/soundrecorder/RecordFragment.java
+++ SoundRecorder/src/com/example/soundrecorder/RecordFragment.java
@@ -1,14 +1,23 @@
 /**
- * 
+ * A simple recording interface to create new sound recordings
  */
 package com.example.soundrecorder;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * @author navin
  *
  */
 public class RecordFragment extends Fragment {
-
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.fragment_record, container, false);
+	}
 }
